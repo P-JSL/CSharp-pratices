ea705faa547eabd8ec8bfc07c90331772964bfc0
P-JSL <althsuwpfl@gmail.com>
2022-06-28 09:54:31 +0900

    C-properties ect.

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ callByValRef refs = new callByValRef();
 Console.WriteLine("입력하신 정수의 값은 {0}입니다.",refs.CallByValueDemo(a));*/
 
 /*참조에 의한 호출*/
-
+/*
 int x = 1;
 int y = 2;
 callByValRef refs2 = new callByValRef();
@@ -46,14 +46,14 @@ Console.WriteLine("y의 값은 {0} 입니다.",y);
 
 Console.WriteLine();
 
-//변수초기화
+--변수초기화
 x = 1;
 y = 2;
 refs2.SwapNum_2(ref x,ref y);
 Console.WriteLine("x의 값은 {0} 입니다.",x);
 Console.WriteLine("y의 값은 {0} 입니다.",y);
 
-//결과에 의한 호출
+--결과에 의한 호출
 Console.WriteLine("a에 저장할 정수값을 입력하세요.");
 int a = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine("b에 저장할 정수값을 입력하세요.");
@@ -61,4 +61,17 @@ int b = Convert.ToInt16(Console.ReadLine());
 
 refs2.GetNumbers(out a,out b);
 Console.WriteLine("x의 값은 {0} 입니다.",a);
-Console.WriteLine("y의 값은 {0} 입니다.",b);
+Console.WriteLine("y의 값은 {0} 입니다.",b);*/
+/*
+  -- 재귀함수
+recursive_method recur = new recursive_method();
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("{0}! = {1}",a,recur.Factorial(a));*/
+
+Person p = new Person();
+Console.WriteLine("이름을 입력하세요.");
+p.Name = Console.ReadLine();
+Console.WriteLine("나이를 입력하세요.");
+p.Age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("안녕하세요 {0}씨, 당신의 나이는 {1}살이군요",p.Name,p.Age);
---
+++ recursive_method.cs
@@ -0,0 +1,72 @@
+﻿namespace consoleApp_pratices;
+
+public class recursive_method
+{
+    public int Factorial(int n)
+    {
+        if (n == 1)
+        {
+            return n;
+        }
+        else
+        {
+            return n * Factorial(--n);
+        }
+    }
+    //method overloading
+    void Print(int a)
+    {
+        Console.WriteLine("입력된 정수값은 {0}입니다.",a);
+    }
+
+    void Print(double a)
+    {
+        Console.WriteLine("입력된 실수값은 {0}입니다.",a);
+    }
+}
+
+//properties
+class Person
+{
+    private String name = "James";
+    private int age;
+    /*
+     * public String Name {set; get;} = "James";
+     * 
+     */
+    public String Name
+    
+    {
+        get { return name;}
+        set
+        {
+            if (value.Length == 0)
+            {
+                throw new ArgumentException("이름이 입력되지 않았습니다.");
+            }
+            else
+            {
+                name = value;
+            }
+        }
+    }
+
+    public int Age
+    {
+        set
+        {
+            age = value;
+        }
+        get
+        {
+            if (age <= 0)
+            {
+                throw new ArgumentException("나이의 입력이 올바르지 않습니다.");
+            }
+            else
+            {
+                return age;
+            }
+        }
+    }
+}
